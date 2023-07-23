fb161174ed7a670208e339877dd49a0066769868
Mendozaa17 <humbertomendoza2017@gmail.com>
2023-07-22 20:41:05 -0500

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Navbar from "./components/Navbar.svelte";
+  import Hero from "./components/Hero.svelte";
+</script>
+
+<main>
+  <Navbar />
+  <Hero />
+</main>
+
+<style>
+  
+</style>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,36 @@
+<script>
+</script>
+
+<nav>
+    <h2>ETXDREW</h2>
+  
+    <ul>
+    <a href="/"><li>Home</li></a>
+    <a href="#About"><li>About</li></a>
+    <a href="#Location"><li>Location</li></a>
+  </ul>
+</nav>
+
+<style>
+  nav {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-direction: row;
+    width: 100%;
+  }
+
+  h2 {
+
+    font-size: 2.6rem;
+  }
+
+  a {
+    display: inline-block;
+    margin: 1rem;
+    font-size: 2.5rem;
+    text-decoration: none;
+    list-style: none;
+  }
+
+</style>
